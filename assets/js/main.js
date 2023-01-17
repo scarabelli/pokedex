ac9571416ec372b0f6799797d9b9b8aa99d88058
scarabelli <gabrielascarabelli@gmail.com>
2023-01-17 20:03:17 -0300

    configurando a lista com os nomes dos pokemons de forma dinâmica

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,32 @@
-console.log('deu bom');
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+function pokemonToHtml(pokemon) {
+    return `
+        <li class="pokemon-line">
+            <span class="pok-number">#001</span>
+            <span class="pok-name">${pokemon.name}</span> 
+            <div class="detail">
+                <ol class="caracteristics">
+                    <li class="type">grass</li>
+                    <li class="type">poison</li>
+                </ol>
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+            </div>    
+        </li>
+    `
+}
+const pokemonList = document.getElementById('pokeList')
+
+
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => {
+       for (let i = 0; i < pokemons.length; i++) {
+        const pokemon = pokemons[i];
+        pokemonList.innerHTML += pokemonToHtml(pokemon)
+       }
+    })
+    .catch((error) => console.log(error))
